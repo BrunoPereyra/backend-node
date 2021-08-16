94df1f9fbad499fbab819f4918b1da5825200c00
unknown <you@example.com>
2021-08-16 17:01:19 -0300

    v6

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 require("colors")
 
-const conectionString = process.env.DB
+const conectionString = "mongodb+srv://Bruno:UyEYM4rUvyB$TMQ@cluster0.kmdpr.mongodb.net/app-instagram?retryWrites=true&w=majority"
 
 mongoose.connect(conectionString, {
     useNewUrlParser: true,
